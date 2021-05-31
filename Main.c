02c3a53bbe472d0fdcda2f721c758d82eaae63c4
Jirayus Jantakerd <jobbysmile@gmail.com>
2021-05-31 10:00:51 +0700

    First time commit

---
+++ Main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   Main.c
+ * Author: Jirayus.j
+ *
+ * Created on May 31, 2021, 9:59 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
